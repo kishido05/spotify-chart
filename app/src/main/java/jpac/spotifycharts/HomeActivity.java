81b2f3948e54295bc5db3b4e9d302acc8d154a2e
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-09-24 09:03:39 +0800

    Create main activity class and layout

---
+++ app/src/main/java/jpac/spotifycharts/HomeActivity.java
@@ -0,0 +1,16 @@
+package jpac.spotifycharts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by syspaulo on 9/24/2015.
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
